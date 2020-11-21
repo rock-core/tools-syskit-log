6b6c8cf35b7fd0f0634c4954c208d6a7365616ac
Sylvain <sylvain.joyeux@tidewise.io>
2020-11-21 12:18:44 -0300

    dsl: implement to_daru_frame's `timeout` argument
    It inserts N/A values in-between samples if they did not get updated
    within the 'timeout' argument.

--- test/daru/frame_builder_test.rb
+++ test/daru/frame_builder_test.rb
@@ -20,33 +20,7 @@ module Syskit
                     end
                 end
 
-                it "guesses the time field if the type is a compound and "\
-                   "has a field of type /base/Time" do
-                    compound_t = @registry.create_compound "/C" do |c|
-                        c.time = "/base/Time"
-                    end
-
-                    builder = FrameBuilder.new(compound_t)
-                    assert_equal ".time.microseconds", builder.time_field.name
-                end
-
-                it "silently ignores if the compound has no time fields" do
-                    compound_t = @registry.create_compound "/C" do |c|
-                        c.time = "/uint64_t"
-                    end
-
-                    builder = FrameBuilder.new(compound_t)
-                    assert_nil builder.time_field
-                end
-
-                it "does not attempt to guess a time field for an array" do
-                    array_t = @registry.create_array @base_time_t, 5
-
-                    builder = FrameBuilder.new(array_t)
-                    assert_nil builder.time_field
-                end
-
-                it "extracts the data and fills the column with it" do
+                it "extracts data into a column" do
                     samples = mock_samples do |i|
                         @compound_t.new(
                             time: { microseconds: i * 1_000_000 + 3 },
@@ -60,7 +34,7 @@ module Syskit
                     start_time = Time.at(0, 3)
                     frame = builder.to_daru_frame(start_time, samples)
 
-                    assert_equal (1..10).to_a, frame.index.to_a
+                    assert_equal (0..9).to_a, frame.index.to_a
                     assert_equal (1..10).to_a, frame[".value"].to_a
                 end
 
@@ -78,7 +52,7 @@ module Syskit
                     start_time = Time.at(0, 3)
                     frame = builder.to_daru_frame(start_time, samples)
 
-                    assert_equal (1..10).to_a, frame.index.to_a
+                    assert_equal (0..9).to_a, frame.index.to_a
                     assert_equal (1..10).to_a, frame["name"].to_a
                 end
 
@@ -96,11 +70,11 @@ module Syskit
                     start_time = Time.at(0, 3)
                     frame = builder.to_daru_frame(start_time, samples)
 
-                    assert_equal (1..10).to_a, frame.index.to_a
+                    assert_equal (0..9).to_a, frame.index.to_a
                     assert_equal (4..40).step(4).to_a, frame[".value"].to_a
                 end
 
-                it "builds a frame using a defined time field" do
+                it "converts to seconds and centers columns declared as time fields" do
                     samples = mock_samples do |i|
                         @compound_t.new(
                             time: { microseconds: i * 1_000_000 + 3 },
@@ -109,16 +83,16 @@ module Syskit
                     end
 
                     builder = FrameBuilder.new(@compound_t)
-                    builder.add(&:value)
+                    builder.add_time_field(".time") { |b| b.time.microseconds }
 
                     start_time = Time.at(0, 3)
                     frame = builder.to_daru_frame(start_time, samples)
 
-                    assert_equal (1..10).to_a, frame.index.to_a
-                    assert_equal (2..20).step(2).to_a, frame[".value"].to_a
+                    assert_equal (0..9).to_a, frame.index.to_a
+                    assert_equal (1..10).to_a, frame[".time"].to_a
                 end
 
-                it "uses the sample's logical time if no time field is selected" do
+                it "allow to create a column from the sample's logical time" do
                     compound_t = @registry.create_compound "/C" do |c|
                         c.value = "/uint64_t"
                     end
@@ -128,12 +102,35 @@ module Syskit
                     end
 
                     builder = FrameBuilder.new(compound_t)
+                    builder.add_logical_time
                     builder.add(&:value)
 
                     start_time = Time.at(0, 2)
                     frame = builder.to_daru_frame(start_time, samples)
 
-                    assert_equal (1..10).to_a, frame.index.to_a
+                    assert_equal (0..9).to_a, frame.index.to_a
+                    assert_equal (1..10).to_a, frame["time"].to_a
+                    assert_equal (2..20).step(2).to_a, frame[".value"].to_a
+                end
+
+                it "lets us set the logical time's column name" do
+                    compound_t = @registry.create_compound "/C" do |c|
+                        c.value = "/uint64_t"
+                    end
+
+                    samples = mock_samples do |i|
+                        compound_t.new(value: i * 2)
+                    end
+
+                    builder = FrameBuilder.new(compound_t)
+                    builder.add_logical_time("lg")
+                    builder.add(&:value)
+
+                    start_time = Time.at(0, 2)
+                    frame = builder.to_daru_frame(start_time, samples)
+
+                    assert_equal (0..9).to_a, frame.index.to_a
+                    assert_equal (1..10).to_a, frame["lg"].to_a
                     assert_equal (2..20).step(2).to_a, frame[".value"].to_a
                 end
 
