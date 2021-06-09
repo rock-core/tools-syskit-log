d5627398a45857916e8b5e6808b97a48f39d59f9
Sylvain <sylvain.joyeux@tidewise.io>
2021-06-09 07:07:55 -0300

    add missing require on gsl

--- lib/syskit/log/daru.rb
+++ lib/syskit/log/daru.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require "daru"
+require "gsl"
 require "syskit/log/daru/create_aligned_frame"
 require "syskit/log/daru/frame_builder"
 require "syskit/log/daru/path_builder"
