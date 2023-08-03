309a3c670a8211cc2ace82b8ee8c981a8cd643c3
Sylvain <sylvain.joyeux@tidewise.io>
2023-08-03 11:26:08 -0300

    fix: simplify error handling by normalizing pocolog files in groups
    This commit uses the (valid) assumption that a data stream,
    once encountered, will always be logged by the same logger in the
    same "file group" (that is, the same sequence of rotated logs).
    
    So, if we process all log files of a group `NAME.SEQ.log` together,
    recovering from an error is simply removing all output files created
    during the processing. This removes the need for a log of bookkeeping.

--- test/datastore/normalize_test.rb
+++ test/datastore/normalize_test.rb
@@ -176,12 +176,11 @@ module Syskit::Log
                         create_logfile_stream "stream0",
                                               metadata: Hash["rock_task_name" => "task0", "rock_task_object_name" => "port"]
                         write_logfile_sample base_time + 3, base_time + 30, 3
+                        write_logfile_sample base_time + 4, base_time + 40, 4
                     end
                     file0_path = logfile_pathname("file0.0.log")
                     file0_size = file0_path.stat.size
-                    logfile_pathname("file0.0.log").open("a") do |io|
-                        io.truncate(file0_size - 1)
-                    end
+                    logfile_pathname("file0.0.log").truncate(file0_size - 1)
                     logfile_pathname("normalized").mkpath
                     reporter = flexmock(Pocolog::CLI::NullReporter.new)
                     flexmock(reporter).should_receive(:current).and_return(10)
@@ -189,12 +188,16 @@ module Syskit::Log
                             .with(/^file0.0.log looks truncated/)
                             .once
                     reporter.should_receive(:current=).with(10 + file0_size - 1).once
-                    error, ios = normalize.normalize_logfile(
+                    normalize.normalize_logfile(
                         logfile_pathname("file0.0.log"),
                         logfile_pathname("normalized"), reporter: reporter
                     )
-                    assert_nil error
-                    assert_equal logfile_pathname("normalized", "task0::port.0.log"), ios[0].path
+                    stream = open_logfile_stream(
+                        logfile_pathname("normalized", "task0::port.0.log"),
+                        "task0.port"
+                    )
+                    assert_equal [[base_time + 3, base_time + 30, 3]],
+                                 stream.samples.to_a
                 end
             end
         end
