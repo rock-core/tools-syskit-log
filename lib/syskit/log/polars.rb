28c4eb6cb87cec702f6fa499b7bf5de2f2502df4
Sylvain <sylvain.joyeux@tidewise.io>
2024-10-08 11:03:53 -0300

    chore: move polars-specific DSL functionality into its own file

--- lib/syskit/log/polars.rb
+++ lib/syskit/log/polars.rb
@@ -7,3 +7,6 @@ require "syskit/log/polars/column_resolved_field_builder"
 require "syskit/log/polars/column_logical_time_builder"
 require "syskit/log/polars/path_builder"
 require "syskit/log/polars/create_aligned_frame"
+require "syskit/log/polars/dsl"
+
+Syskit::Log::DSL.include(Syskit::Log::Polars::DSL)
