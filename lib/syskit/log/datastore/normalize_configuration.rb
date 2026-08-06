73cd6e7cffb3968f534887ba4eebf65b05f66192
Sylvain Joyeux <sylvain.joyeux@tidewise.io>
2026-08-06 11:30:34 -0300

    feat: allow rejecting samples with duplicate rt/lg times via the config file
    One more refactor of the config file structure, but that's worth it.

--- lib/syskit/log/datastore/normalize_configuration.rb
+++ lib/syskit/log/datastore/normalize_configuration.rb
@@ -5,7 +5,7 @@ module Syskit
         # Object holding the configuration data for the normalization proces
         class NormalizeConfiguration
             def initialize
-                @stream_metadata_per_type = {}
+                @stream_config_per_type = {}
                 @schema = File.join(__dir__, "normalize_configuration.schema.json")
             end
 
@@ -21,41 +21,74 @@ module Syskit
 
             # Add configuration from hash (usually from a YAML file)
             def update_from_hash(config)
-                JSON::Validator.validate!(@schema, config)
+                JSON::Validator.validate!(@schema, config, insert_defaults: true)
 
-                config.dig("metadata", "streams")&.each do |entry|
+                config.dig("streams").each do |entry|
                     if (typename = entry.dig("match", "type"))
-                        @stream_metadata_per_type[typename] = MetadataUpdate.new([entry])
+                        @stream_config_per_type[typename] = StreamConfig.new(
+                            lg_allow_duplicates: entry["lg_allow_duplicates"],
+                            rt_allow_duplicates: entry["rt_allow_duplicates"],
+                            metadata_ops: [entry["metadata"]]
+                        )
                     end
                 end
             end
 
-            # Return a metadata update object for the given type
+            # Return a stream configuration object for the given type
             #
-            # @return MetadataUpdate
-            def stream_metadata_update_for_type(type_name)
-                @stream_metadata_per_type[type_name] || MetadataUpdate.identity
+            # @return StreamConfig
+            def stream_config_for_type(type_name)
+                @stream_config_per_type[type_name] ||= StreamConfig.new
             end
 
             # Encapsulation of metadata update operations
-            class MetadataUpdate
-                def initialize(ops = [])
-                    @ops = ops.dup
+            class StreamConfig
+                def initialize(
+                    lg_allow_duplicates: true, rt_allow_duplicates: true,
+                    metadata_ops: []
+                )
+                    @metadata_ops = metadata_ops.dup
+                    @lg_allow_duplicates = lg_allow_duplicates
+                    @rt_allow_duplicates = rt_allow_duplicates
                 end
 
-                def add(update)
-                    @ops << update
+                # Update {#lg_allow_duplicates?}
+                attr_writer :lg_allow_duplicates
+
+                # Whether the importer should reject samples that have the same logical
+                # time than the previous sample
+                #
+                # The default is to accept such samples, as it can happen "naturally"
+                # with Rock's logger
+                #
+                # @see lg_allow_duplicates= rt_allow_duplicates?
+                def lg_allow_duplicates?
+                    @lg_allow_duplicates
                 end
 
-                def self.identity
-                    MetadataUpdate.new
+                # Update {#lg_allow_duplicates?}
+                attr_writer :rt_allow_duplicates
+
+                # Whether the importer should reject samples that have the same real
+                # time than the previous sample
+                #
+                # The default is to accept such samples, as it can happen "naturally"
+                # with Rock's logger
+                #
+                # @see rt_allow_duplicates= lg_allow_duplicates?
+                def rt_allow_duplicates?
+                    @rt_allow_duplicates
                 end
 
-                def update(metadata)
-                    return metadata if @ops.empty?
+                def metadata_add_op(update)
+                    @metadata_ops << update
+                end
+
+                def metadata_update(metadata)
+                    return metadata if @metadata_ops.empty?
 
                     metadata = metadata.dup
-                    @ops.each do |spec|
+                    @metadata_ops.each do |spec|
                         op, k, v = spec.values_at("op", "key", "value")
                         case op
                         when "set"
